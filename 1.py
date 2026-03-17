248eec165ea06f0a4a6dcc36f891c65cb1ea9c21
sak-26-2005 <sakshibalupadwal2005@gmail.com>
2026-03-17 13:29:07 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-n = 5
-for i in range(1, n + 1):
-    # Print leading spaces
-    for j in range(n - i):
-        print(" ", end="")
-    # Print stars
-    for k in range(1, 2 * i):
-        print("*", end="")
-    print() # Move to the next line
